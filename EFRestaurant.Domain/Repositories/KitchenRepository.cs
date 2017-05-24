ada6a12487f2a2e82cb11acfc7af0571ff56750c
mceprnja <mariod@dump.hr>
2017-05-25 01:23:43 +0200

    testing adding new repository

---
+++ EFRestaurant.Domain/Repositories/KitchenRepository.cs
@@ -0,0 +1,20 @@
+﻿using EFRestaurant.Data.Models;
+using EFRestaurant.Data.Models.Entities;
+using System.Linq;
+
+namespace EFRestaurant.Domain.Repositories
+{
+    public class KitchenRepository
+    {
+        public KitchenRepository()
+        {
+            _context = new RestaurantContext();
+        }
+        private readonly RestaurantContext _context;
+
+        public KitchenModel GetKitchenModel(int id)
+        {
+            return _context.KitchenModels.Find(id);
+        }
+    }
+}
