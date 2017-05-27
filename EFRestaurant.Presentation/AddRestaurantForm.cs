a48525ac04abe7184a4e1532723d248c973cb05e
Kresimir Condic <v-kresimir.condic@dump.hr>
2017-05-27 14:51:59 +0200

    Testing multiple context functionality.

--- EFRestaurant.Presentation/AddRestaurantForm.cs
+++ EFRestaurant.Presentation/AddRestaurantForm.cs
@@ -11,10 +11,11 @@ namespace EFRestaurant.Presentation
         {
             InitializeComponent();
             _restaurantRepository = new RestaurantRepository();
+            _kitchenRepository = new KitchenRepository();
             KitchenModelComboBox.SelectedIndex = 0;
         }
         private readonly RestaurantRepository _restaurantRepository;
-
+        private readonly KitchenRepository _kitchenRepository;
         private void OkButtonNewRestaurant_Click(object sender, EventArgs e)
         {          
             if (string.IsNullOrEmpty(NewRestaurantTextBox.Text))
@@ -24,7 +25,8 @@ namespace EFRestaurant.Presentation
                 var restaurantToAdd = new Restaurant()
                 {
                     Name = NewRestaurantTextBox.Text,
-                    KitchenModel = _restaurantRepository.GetKitchenModel(KitchenModelComboBox.SelectedIndex + 1)
+                    KitchenModel=_kitchenRepository.GetKitchenModel(KitchenModelComboBox.SelectedIndex+1)
+                   // KitchenModel = _restaurantRepository.GetKitchenModel(KitchenModelComboBox.SelectedIndex + 1)
                 };
                 _restaurantRepository.AddRestaurant(restaurantToAdd);
                 Close();
